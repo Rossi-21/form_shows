0d91db90f26becc1a57911fc945023a94dbdf1ea
Ross Palmer <rossi21@gmail.com>
2023-11-09 07:55:49 -0800

    second commit

--- user_app/models.py
+++ user_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Show(models.Model):
+    title = models.CharField(max_length=256)
+    network = models.CharField(max_length=256)
+    release_date = models.DateField()
+    description = models.TextField()
+    created_at =  models.DateTimeField(auto_now_add=True, null = True)
+    updated_at = models.DateTimeField(auto_now=True, null= True)
