255f78e104d44bab0662d145cdb5625f4615efe7
Ross Palmer <rossi21@gmail.com>
2023-11-10 10:02:11 -0800

    validation

--- user_app/forms.py
+++ user_app/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.forms import ModelForm
 from .models import Show
+from django.utils import timezone
+
 
 class CreateShowForm(forms.ModelForm):
     class Meta:
@@ -10,9 +12,36 @@ class CreateShowForm(forms.ModelForm):
         fields = ('title', 'network', 'release_date', 'description')
 
         widgets = {
-            'title': forms.TextInput(attrs={'class' : 'form-control'}),
-            'network' : forms.TextInput(attrs={'class' : 'form-control'}),
-            'release_date' : forms.DateInput(attrs={'type' :'date', 'class' : 'form-control'}),
+            'title': forms.TextInput(attrs={'class' : 'form-control mb-2'}),
+            'network' : forms.TextInput(attrs={'class' : 'form-control mb-2'}),
+            'release_date' : forms.DateInput(attrs={'type' :'date', 'class' : 'form-control mb-2'}),
             'description' : forms.Textarea(attrs={'class': 'form-control'})
         }
 
+    def clean(self):
+        cleaned_data = super(CreateShowForm, self).clean()
+
+        title = self.cleaned_data.get('title')
+        network = self.cleaned_data.get('network')
+        release_date = self.cleaned_data.get('release_date')
+        description = self.cleaned_data.get('description')
+
+        if len(title) < 3:
+            self._errors['title'] = self.error_class([
+                'Title must be at least 3 Characters'
+            ])
+        if len(network) < 2:
+            self._errors['network'] = self.error_class([
+                'Network must be at least 2 characters'
+            ])
+        if release_date and release_date > timezone.now().date():
+            self._errors['release_date'] = self.error_class([
+                'Release date cannot be in the future'
+            ])
+
+        if len(description) < 8:
+            self._errors['description'] = self.error_class([
+                'Description must be at least 8 characters'
+            ])
+
+        return cleaned_data
