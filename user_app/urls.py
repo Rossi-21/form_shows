a37dc793a88321bf613dacd49ec9e67cac8a0e0f
Ross Palmer <rossi21@gmail.com>
2023-11-11 07:05:20 -0800

    view show/edit show

--- user_app/urls.py
+++ user_app/urls.py
@@ -4,7 +4,10 @@ from . import views
 urlpatterns = [
 
     path('', views.home, name="home"),
-    path('shows/', views.allShows, name="allShows")
+    path('shows/', views.allShows, name="allShows"),
+    path('shows/<int:id>/', views.viewShow, name="viewShow"),
+    path('shows/<int:id>/edit/', views.editShow, name="editShow"),
+    path('shows/<int:id>/update', views.updateShow, name="updateShow"),
     #path('register', views.registerPage, name="register"),
     #path('login', views.loginPage, name="login"),
     #path('logout/', views.logoutUser, name="logout"),
--- user_app/views.py
+++ user_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import *
-from .forms import CreateShowForm
+from .forms import *
 
 def home(request):
 
@@ -22,7 +22,17 @@ def home(request):
 
     return render(request, "index.html", context)
 
+def viewShow(request, id):
+
+    show = Show.objects.get(id=id)
+    
+    context = {
+        'show' : show
+    }
+    return render(request, 'view.html', context)
+
 def allShows(request):
+
     shows = Show.objects.all()
 
     context = {
@@ -30,4 +40,31 @@ def allShows(request):
     }
     return render(request, "shows.html", context )
 
-# Create your views here.
+def editShow(reqeust, id):
+
+    show = Show.objects.get(id=id)
+    
+    form = UpdateShowForm
+
+    context ={
+        'show' : show,
+        'form' : form
+    }
+
+    return render(reqeust, 'edit.html', context)
+
+def updateShow(request, id):
+
+    show = Show.objects.get(id=id)
+
+    form = UpdateShowForm()
+
+    if request.method == 'POST':
+
+        form = UpdateShowForm(request.POST)
+
+        if form.is_valid:
+
+            form.save()
+
+        return redirect(f'/shows/{show.id}', show)
