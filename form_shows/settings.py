e8451e62e62103c45560ba61fe7459931fc0a930
Ross Palmer <rossi21@gmail.com>
2023-12-20 06:04:56 -0800

    secret

--- form_shows/settings.py
+++ form_shows/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,7 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6khqiii@2e%l0okwyl*-0cop(%5!(x27!ey-uhp6qq)s!j+&=d'
+SECRET_KEY = config('SECERT_KEY')
+
+API_KEY = config('API_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
