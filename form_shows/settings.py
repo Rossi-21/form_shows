dc9b7bac5acfa4079d51a644aef59317364f62d6
Ross Palmer <rossi21@gmail.com>
2023-12-24 07:44:58 -0800

    comments

--- form_shows/settings.py
+++ form_shows/settings.py
@@ -27,13 +27,13 @@ SECRET_KEY = config('SECERT_KEY')
 
 API_KEY = config('API_KEY')
 
+# Email settings
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'rossi21.dev@gmail.com'
+EMAIL_HOST_USER = config('EMAIL_ADDRESS')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = 'rossi21.dev@gmail.com'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- user_app/views.py
+++ user_app/views.py
@@ -16,26 +16,31 @@ from .forms import *
 
 api_key = settings.API_KEY
 email_password = settings.EMAIL_HOST_PASSWORD
+email_address = settings.EMAIL_ADDRESS
 
+# Register user function
 def registerUser(request):
-
+    # Display the user creation form
     form = CreateUserForm()
 
     if request.method == 'POST':
+        # Grab the submitted info from the form
         form = CreateUserForm(request.POST)
 
         if form.is_valid():
+            # Save the user & log them in
             user = form.save()
             login(request, user)
-
+            # Send the new user and email
             subject = 'Welcome to the TV Party!'
             html_message = render_to_string('registration_email.html', {'user': user})
             plain_message = strip_tags(html_message)
-            from_email = 'rossi21.dev@gmail.com'
+            from_email = 'email_address'
             to = [user.email]
 
             send_mail(subject, plain_message, from_email, to, html_message=html_message )
 
+            #Rredirect them to the dashboard
             return redirect('allShows')
     
     context = {
@@ -44,41 +49,50 @@ def registerUser(request):
 
     return render(request, 'register.html', context)
 
+# Login user function
 def loginUser(request):
 
     if request.method  == "POST":
-
+        # Get the username and password from the database
         username = request.POST.get('username')
         password = request.POST.get('password')
-
+        # Verify the username and password
         user = authenticate(request, username = username, password = password)
 
         if user is not None:
+            # If the user exists log them in
             login(request, user)
-
+            # Redirect to the dashboard
             return redirect('allShows')
         
         else:
+            # If the user does not exist, display an error message
             messages.info(request, 'Username or Password is incorrect')       
 
     return render(request, 'login.html')
 
+# Display user info page
 @login_required
 def showUser(request, id):
-
+    # Get the user & the profile,shows and favorites associated with the user
     user = User.objects.get(id=id)
     profile = Profile.objects.get(id=request.user.id)
     shows = Show.objects.filter(user=user)
     favorites = Show.objects.filter(like=user)
 
+    # Allow other users to follow our unfollow the user displayed on the page
     if request.method == "POST":
+        # Create a veriable for the Logged In user
         current_user_profile = request.user.profile
+        # Create a veriable for the form action
         action = request.POST['follow']
+        # If unfollow, remove the viewed user from the Logged In users follows list
         if action == "unfollow":
             current_user_profile.follows.remove(user.id)
+        # If follow, add the viewed user to the Logged In users follows list
         elif action == "follow":
             current_user_profile.follows.add(user.id)
-        
+        # Save the Logged in users profile
         current_user_profile.save()
 
     context = {
